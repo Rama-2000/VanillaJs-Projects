d26b80f2f2d3717b1144cb27aa3bf5a53a41bcf7
Rakhmatullokh <rahmatullo9984@gmail.com>
2023-09-05 15:02:13 +0500

    FAQ PART

---
+++ FAQ Part/app.js
@@ -0,0 +1,28 @@
+// Traversing the DOM
+// const btns = document.querySelectorAll('.question-btn')
+
+// btns.forEach(function(btn){
+//     btn.addEventListener('click',function(e){
+//         const question = e.currentTarget.parentElement.parentElement
+//         question.classList.toggle('show-text')
+//     })
+// })
+
+
+
+// Using Selectors inside the element
+const questions = document.querySelectorAll('.question')
+
+questions.forEach(function(question){
+    const btn = question.querySelector('.question-btn')
+    btn.addEventListener('click', function(){
+        questions.forEach(function(item){
+            if(item !== question){
+                item.classList.remove('show-text')
+            }
+        })
+
+
+        question.classList.toggle('show-text')
+    })
+})
